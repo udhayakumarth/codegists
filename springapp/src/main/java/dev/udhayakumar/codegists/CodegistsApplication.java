0ddad7ad293e93d00256b97937a2cfadf347fa24
Udhayakumar <dev.udhayakumar@gmail.com>
2025-02-07 11:10:23 +0530

    Added apringapp

---
+++ springapp/src/main/java/dev/udhayakumar/codegists/CodegistsApplication.java
@@ -0,0 +1,13 @@
+package dev.udhayakumar.codegists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodegistsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodegistsApplication.class, args);
+	}
+
+}
---
+++ springapp/src/test/java/dev/udhayakumar/codegists/CodegistsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.udhayakumar.codegists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodegistsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
